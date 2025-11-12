b4cca0fd5dc866a7bd96bf37c3481dc6365cc771
stingmon <code.stingmon@gmail.com>
2025-11-12 13:46:50 +0900

    Docs: week4 제출

---
+++ week4/src/Counter.js
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+import CountNumber from './CountNumber';
+
+function Counter() {
+    const [count, setCount] = useState(0);
+
+    function increment() {
+        setCount(count + 1);
+    }
+
+    function decrement() {
+        setCount(count - 1);
+    }
+
+    function reset() {
+        setCount(count - count);
+    }
+
+    function increment10() {
+        setCount(count + 10);
+    }
+
+    function decrement10() {
+        setCount(count - 10);
+    }
+
+    return (
+        <div>
+            <CountNumber count={count} />
+            <button onClick={decrement10}>-10</button>
+            <button onClick={decrement}>-1</button>
+            <button onClick={reset}>reset</button>
+            <button onClick={increment}>+1</button>
+            <button onClick={increment10}>+10</button>
+        </div>
+    )
+}
+export default Counter;
